ef2395cdddf8f36819451a2d2773c278de69327e
Emanuele Mazzante <emanuele78m@gmail.com>
2019-04-06 19:50:09 +0200

    Removed unused controllers/routes and vue components

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,15 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+	
+	Route::get(
+	  '/', function () {
+		return view('welcome');
+	});
+	
+	Route::namespace('Auth')->group(
+	  function () {
+		  Route::post('/registration', 'RegisterController@register');
+		  Route::post('/login', 'LoginController@login');
+		  Route::post('/logout', 'LoginController@logout')->middleware('auth')->name('logout');
+		  Route::get('/login', 'LoginController@showLoginForm')->name('login');
+		  Route::get('/registrazione', 'RegisterController@showRegistrationForm')->name('register');
+	  });
