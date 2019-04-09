6b7b31f2d63f752d6c5a7352c423313a5aaf5af6
Emanuele Mazzante <emanuele78m@gmail.com>
2019-04-09 10:30:33 +0200

    Moved project config value from project.php to .env file

--- config/project.php
+++ config/project.php
@@ -2,13 +2,9 @@
 	
 	return [
 	  
-	  'token_endpoint_production' => 'https://emanuelemazzante.dev/portfolio/mybnb/api/new-token',
-	  'token_endpoint_development' => 'http://127.0.0.1:8000/api/new-token',
-	  'cities_endpoint_production' => 'https://emanuelemazzante.dev/portfolio/mybnb/api/cities',
-	  'cities_endpoint_development' => 'http://127.0.0.1:8000/api/cities',
-	  'use_test_recipient' => true,
-	  'test_recipient_mail' => 'emanuele78m@gmail.com',
-	  'token_key' => 'app-token',
-	  'token_expiration_time' => 20
+	  'use_test_recipient' => env('USE_TEST_RECIPIENT', true),
+	  'test_recipient_mail' => env('TEST_RECIPIENT_MAIL', 'john@doe.com'),
+	  'token_key' => env('APP_TOKE_KEY'),
+	  'token_expiration_time' => env('TOKEN_EXPIRATION_TIME', 20),
 	
 	];
