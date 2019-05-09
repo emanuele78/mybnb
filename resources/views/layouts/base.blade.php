f9becacec6504a036238eb6cc1e534e692cbe563
Emanuele Mazzante <emanuele78m@gmail.com>
2019-05-09 18:36:37 +0200

    Modified script for cookie

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -9,7 +9,7 @@
         <link rel="stylesheet" href="{{asset('css/plain.css')}}">
         <script type="text/javascript">
             var _iub = _iub || [];
-            _iub.csConfiguration = {"consentOnContinuedBrowsing":false,"lang":"it","siteId":1583375,"priorConsent":false,"cookiePolicyId":12084778, "banner":{ "position":"top" } };
+            _iub.csConfiguration = {"consentOnContinuedBrowsing":true,"lang":"it","siteId":1583375,"priorConsent":false,"cookiePolicyId":12084778, "banner":{ "position":"top" } };
         </script><script type="text/javascript" src="//cdn.iubenda.com/cs/iubenda_cs.js" charset="UTF-8" async> </script>
         <title>MyBnB | Emanuele Mazzante Portfolio</title>
     </head>
