8ddc6dd1138921dcc4f599a9250da4d517f09891
Emanuele Mazzante <emanuele78m@gmail.com>
2019-05-15 17:48:49 +0200

    Added fontawesome style

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,13 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         @stack('meta')
+        <link href="{{asset('font/css/all.css')}}" rel="stylesheet">
         @stack('styles')
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <link rel="stylesheet" href="{{asset('css/plain.css')}}">
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
+        <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png')}}">
+        <link rel="manifest" href="{{asset('site.webmanifest')}}">
         <script type="text/javascript">
             var _iub = _iub || [];
             _iub.csConfiguration = {"consentOnContinuedBrowsing":true,"lang":"it","siteId":1583375,"priorConsent":false,"cookiePolicyId":12084778, "banner":{ "position":"top" } };
